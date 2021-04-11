fd5353e20a838a04c7c00248e0355db66605ee7d
Jayant Dwivedi <jayantdwivedi19@gmail.com>
2021-04-11 20:03:24 +0530

    Repos with dummy data

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,9 +1,97 @@
-import React from 'react';
-import styled from 'styled-components';
-import { GithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import React from "react";
+import styled from "styled-components";
+import { GithubContext } from "../context/context";
+import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const { repos } = React.useContext(GithubContext);
+  // console.log(repos);
+
+  let languages = repos.reduce((total, item) => {
+    // console.log(item);
+    // destructure items
+
+    const { language, stargazers_count } = item;
+
+    if (!language) return total;
+    // console.log(language);
+
+    if (!total[language]) {
+      total[language] = { label: language, value: 1, stars: stargazers_count };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+        stars: total[language].stars + stargazers_count,
+      };
+    }
+
+    return total;
+  }, {});
+
+  // console.log(languages);
+
+  // sorting 5 highest value first adding slice
+  const mostUsed = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
+  console.log(languages);
+
+  // most stars per languages
+  const mostPopular = Object.values(languages)
+    .sort((a, b) => {
+      return b.stars - a.stars;
+    })
+    .map((item) => {
+      return { ...item, value: item.stars };
+    })
+    .slice(0, 5);
+
+  // console.log(mostPopular);
+
+  // Stars, Forks
+  let { stars, forks } = repos.reduce(
+    (total, item) => {
+      const { stargazers_count, name, forks } = item;
+      total.stars[stargazers_count] = { label: name, value: stargazers_count };
+      total.forks[forks] = { label: name, value: forks };
+      return total;
+    },
+    { stars: {}, forks: {} }
+  );
+
+  stars = Object.values(stars).slice(-5).reverse();
+  forks = Object.values(forks).slice(-5).reverse();
+
+  // console.log(stars);
+
+  // Dummy Data
+  const chartData = [
+    {
+      label: "HTML",
+      value: "12",
+    },
+    {
+      label: "CSS",
+      value: "34",
+    },
+    {
+      label: "Javascript",
+      value: "18",
+    },
+  ];
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        {/* <ExampleChart data={chartData} /> */}
+        <Pie3D data={mostUsed} />
+        <Column3D data={stars} />
+        <Doughnut2D data={mostPopular} />
+        <Bar3D data={forks} />
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
